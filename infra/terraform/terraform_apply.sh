f32eae614f7f3ffcbd5c7c4ce54c17b9dd079eae
Marcelo Feitoza <marcelo.feitoza@sou.inteli.edu.br>
2024-05-15 18:14:45 -0300

    testing terraform

--- infra/terraform/terraform_apply.sh
+++ infra/terraform/terraform_apply.sh
@@ -13,7 +13,7 @@ check_terraform_state() {
     terraform state list | grep -q $1
 }
 
-# Checar e importar o ELB se ele já existir e não estiver no estado do Terraform
+# Checar se o ELB já existe e não está no estado do Terraform
 if aws elb describe-load-balancers --load-balancer-names "$ELB_NAME" 2>&1 | grep -q 'LoadBalancerName'; then
     if ! check_terraform_state "aws_elb.web_elb"; then
         echo "ELB já existe, importando..."
@@ -23,7 +23,7 @@ if aws elb describe-load-balancers --load-balancer-names "$ELB_NAME" 2>&1 | grep
     fi
 fi
 
-# Checar e importar o Key Pair se ele já existir e não estiver no estado do Terraform
+# Checar se o Key Pair já existe e não está no estado do Terraform
 if aws ec2 describe-key-pairs --key-names "$KEY_PAIR_NAME" 2>&1 | grep -q 'KeyPairId'; then
     if ! check_terraform_state "aws_key_pair.deployer_key_pair"; then
         echo "Key Pair já existe, importando..."
@@ -33,12 +33,11 @@ if aws ec2 describe-key-pairs --key-names "$KEY_PAIR_NAME" 2>&1 | grep -q 'KeyPa
     fi
 fi
 
-# Verifica o plano de execução do Terraform
+# Executar plano e aplicação apenas se necessário
 echo "Verificando plano do Terraform..."
 plan_output=$(terraform plan -var="db_password=${DB_PASSWORD}" -var="public_key=${PUBLIC_KEY}" -no-color)
 echo "$plan_output"
 
-# Executar o Terraform Apply apenas se necessário
 if echo "$plan_output" | grep -q 'No changes'; then
     echo "Nenhuma mudança necessária."
 else
